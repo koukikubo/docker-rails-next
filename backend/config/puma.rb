2c465dce176a209899ed61c9ebddefce94e9bbd9
koukikubo <kouki.xx.0817@gmail.com>
2025-02-24 14:54:27 +0900

    最新の変更をコミット

--- backend/config/puma.rb
+++ backend/config/puma.rb
@@ -1,24 +1,21 @@
-
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS", max_threads_count)
 threads min_threads_count, max_threads_count
 
-rails_env = ENV.fetch("RAILS_ENV") { "development" }
+rails_env = ENV.fetch("RAILS_ENV", "development")
 
 if rails_env == "production"
-  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { 1 })
-  if worker_count > 1
-    workers worker_count
-  else
-    preload_app!
-  end
+  worker_count = ENV.fetch("WEB_CONCURRENCY", 1).to_i
+  workers worker_count if worker_count > 1
+  preload_app!
 end
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+
+worker_timeout 3600 if rails_env == "development"
+
 port ENV.fetch("PORT", 3000)
 bind "tcp://0.0.0.0:#{ENV.fetch("PORT", 3000)}"
 
 environment rails_env
-
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+pidfile ENV.fetch("PIDFILE", "tmp/pids/server.pid")
 
 plugin :tmp_restart
