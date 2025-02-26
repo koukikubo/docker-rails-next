6d3d383f8cef01840bf27445f27e46a9cd21eb1d
koukikubo <kouki.xx.0817@gmail.com>
2025-02-27 08:30:47 +0900

    🍻 バックエンドのデータをフロントで表示

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<{ id: number; name: string }[]>([]);
 
   useEffect(() => {
     console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
@@ -11,18 +11,19 @@ export default function Home() {
     fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/users`)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        console.log("Fetched Data:", data);
         setUsers(data);
       })
       .catch((err) => console.error("Fetch error:", err));
   }, []);
-  
+
+  console.log("Current Users State:", users);
 
   return (
     <div>
       <h1>ユーザーの一覧</h1>
       <ul>
-        {users.map((user: { id: number; name: string }) => (
+        {users?.map((user) => (
           <li key={user.id}>{user.name}</li>
         ))}
       </ul>
