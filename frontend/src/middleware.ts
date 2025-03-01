ad2f24d55338ed2408ae74740bf9c2cc953fb2ac
koukikubo <kouki.xx.0817@gmail.com>
2025-03-01 11:42:38 +0900

    本番環境対応化

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -8,26 +8,27 @@ export function middleware(req: NextRequest) {
       status: 401,
       headers: {
         "WWW-Authenticate": 'Basic realm="Secure Area"',
+        "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
       },
     });
   }
 
-  // Basic 認証の解析
   const base64Credentials = authHeader.split(" ")[1];
   const [username, password] = atob(base64Credentials).split(":");
 
-  const validUsername = process.env.NEXT_PUBLIC_BASIC_AUTH_USER;
-  const validPassword = process.env.NEXT_PUBLIC_BASIC_AUTH_PASSWORD;
+  // 環境変数を `NEXT_PUBLIC_` なしに変更
+  const validUsername = process.env.BASIC_AUTH_USER;
+  const validPassword = process.env.BASIC_AUTH_PASSWORD;
 
   if (username !== validUsername || password !== validPassword) {
     return new NextResponse("Unauthorized", {
       status: 401,
       headers: {
         "WWW-Authenticate": 'Basic realm="Secure Area"',
+        "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
       },
     });
   }
 
   return NextResponse.next();
 }
-
