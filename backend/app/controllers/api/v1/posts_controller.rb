30552a86afa412a9860dea00c46956c9f6978edc
koukikubo <kouki.xx.0817@gmail.com>
2025-03-24 15:58:41 +0900

    コントローラーの修正

--- backend/app/controllers/api/v1/posts_controller.rb
+++ backend/app/controllers/api/v1/posts_controller.rb
@@ -1,46 +1,49 @@
-def index
-  @posts = Post.all.order(created_at: :desc)
-  render json: @posts.map { |post| serialize_post(post) }
-end
-
-def show
-  @post = Post.find(params[:id])
-  render json: serialize_post(@post)
-end
-
-def create
-  @post = Post.new(post_params)
-  if @post.save
-    render json: serialize_post(@post), status: :created
-  else
-    render json: @post.errors, status: :unprocessable_entity
+class Api::V1::PostsController < ApplicationController
+  def index
+    @posts = Post.all.order(created_at: :desc)
+    render json: @posts.map { |post| serialize_post(post) }
   end
-end
-
-def update
-  @post = Post.find(params[:id])
-  if @post.update(post_params)
+  
+  def show
+    @post = Post.find(params[:id])
     render json: serialize_post(@post)
-  else
-    render json: @post.errors, status: :unprocessable_entity
   end
-end
-
-private
-
-def serialize_post(post)
-  {
-    id: post.id,
-    title: post.title,
-    content: post.content,
-    user_id: post.user_id,
-    created_at: post.created_at,
-    updated_at: post.updated_at,
-    image_url: post.image.attached? ? url_for(post.image) : nil,
-    movie_url: post.movie.attached? ? url_for(post.movie) : nil,
-  }
-end
-
-def post_params
-  params.require(:post).permit(:user_id, :title, :content, :image, :movie)
+  
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      render json: serialize_post(@post), status: :created
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      render json: serialize_post(@post)
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+  
+  private
+  
+  def serialize_post(post)
+    {
+      id: post.id,
+      title: post.title,
+      content: post.content,
+      user_id: post.user_id,
+      created_at: post.created_at,
+      updated_at: post.updated_at,
+      image_url: post.image.attached? ? url_for(post.image) : nil,
+      movie_url: post.movie.attached? ? url_for(post.movie) : nil,
+    }
+  end
+  
+  def post_params
+    params.require(:post).permit(:user_id, :title, :content, :image, :movie)
+  end
+  
 end
