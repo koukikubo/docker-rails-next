e24a32e2395d72a6254e8f36765f8c1b32d787d9
koukikubo <kouki.xx.0817@gmail.com>
2025-03-28 11:58:34 +0900

    uidを追加するため修正

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_16_053348) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_28_024034) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_16_053348) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
